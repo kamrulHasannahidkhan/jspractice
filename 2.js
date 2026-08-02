472262c8776b54224657e163899436dcb2840268
kamrulHasannahidkhan <134895166+kamrulHasannahidkhan@users.noreply.github.com>
2026-08-02 18:48:52 +0600

    2nd

---
+++ 2.js
@@ -0,0 +1,6 @@
+function isPalindrome(str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanStr === cleanStr.split('').reverse().join('');
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
