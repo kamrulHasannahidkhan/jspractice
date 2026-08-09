71636def8be0a5681950d23093347a58323e50b5
kamrulHasannahidkhan <134895166+kamrulHasannahidkhan@users.noreply.github.com>
2026-08-09 23:57:18 +0600

    7

---
+++ 15.js
@@ -0,0 +1,7 @@
+function areAnagrams(str1, str2) {
+  const format = str => str.toLowerCase().replace(/[^a-z0-9]/g, '').split('').sort().join('');
+  return format(str1) === format(str2);
+}
+
+console.log(areAnagrams("listen", "silent")); // true
+console.log(areAnagrams("hello", "world"));   // false
