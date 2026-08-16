858ac6992b71a0af4a6a7ecef2bbf420b1b5dcda
kamrulHasannahidkhan <134895166+kamrulHasannahidkhan@users.noreply.github.com>
2026-08-16 20:18:28 +0600

    7

---
+++ 22.js
@@ -0,0 +1,13 @@
+function throttle(func, limit) {
+  let inThrottle;
+  return function (...args) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+// Usage Example
+const logScroll = throttle(() => console.log("Scrolled"), 1000);
